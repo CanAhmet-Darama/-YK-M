520b764bba6385de88e1cd107453d58570963318
Can Ahmet Darama <cadarama3gdvlp@gmail.com>
2023-09-23 06:26:17 +0300

    Sunuk 28
    İstatistikler ve hesaplama bilgileri eklendi. Tarihe birçok ayrıntı eklendi. Vdalronk istilası gözden geçiriliyor

---
+++ İstatistikler.cs
@@ -0,0 +1,164 @@
+﻿class Nüfuslar_2263 : 484 M{
+    class Uluslar
+    {
+        class Kuzey_Sideranz : 126 M
+        {
+            Crawonz :  24 M
+            Egrant :  9 M
+            Powrin :  5 M
+            Sreva :  5 M
+            Arenx :  6 M
+            Aumbor : 2 M
+            Targalf :  3 M
+            Sibredix :  7 M
+            Mowrac :  6 M
+            Vurnesk :  6 M
+            Garujen :  5 M
+            Dunslab :  5 M
+            Grodaus :  2 M
+            Gritz :  8 M
+            Lerespon : 16 M
+            Grund :  8 M
+            Bezartax : 7 M
+            Putsura:  1 M
+
+        }
+        class Güney_Sideranz: 75 M
+        {
+            Kaburman:  10 M
+            Seproy: 11 M
+            Erezut:  5 M
+            Chopurtan: 7 M
+            Marun:  7 M
+            Tamarug:  5 M
+            Sharuud:  2 M
+            Qurgash:  3 M
+            Gingin:  4 M
+            Obuyhod: 1 M
+            Afkar:  10 M
+            Monayba:  3 M
+            Faputat:  2 M
+            Umrin:  4 M
+            Danglatoy: 1 M
+        }
+        class Hefkirin : 9 M
+        {
+            Kuzey Hefkirin: 7 M
+            Güney Hefkirin: 2 M
+        }
+        class Timbara : 45 M
+        {
+            Senabio :  20 M
+            Jurgolin : 13 M
+            Ziliar :  4 M
+            Oriagel :  8 M
+        }
+        class Nyrokos : 25 M
+        {
+            Yurox :  19 M
+            Olepek :  4 M
+            Sentriz :  2 M
+        }
+        class Onulorp : 166 M
+        {
+            Aleonk :  9 M
+            Luzin :  12 M
+            Belarb :  6 M
+            Epar :  6 M
+            Enjida :  18 M
+            Yawaru :  5 M
+            Chinak :  7 M
+            Shupat :  8 M
+            Chirza :  7 M
+            Iraja :  7 M
+            Drawunad : 7 M
+            Guayn :  3 M
+            Ushtim :  14 M
+            Asuwam :  17 M
+            Jivar :  15 M
+            Azurna :  7 M
+            Tokare :  3 M
+            Kambero :  5 M
+            Putasko :  10 M
+
+        }
+        class Dragzyne : 14 M
+        {
+            Drags :  9 M
+            Erotin :  3 M
+            Puskark :  2 M
+        }
+        class İnsansı : 24 M
+        {
+            Bilvodan :  2 M
+            Erünrim :  1 M
+            Tukrimon :  1 M
+            Shanorach :  3 M
+            Jayen :  2 M
+            Vdalronk / Uzirink :  5 M
+            Mustarok :  4 M
+            Enalek :  4 M
+            Narupza :  2 M
+
+        }
+    }
+    class Şehirler
+    {
+        class Kuzey_Sideranz
+        {
+            class Levri { 
+                Peskora : 700 B
+                Ruxion :
+                Thirio : 350 B
+                Marjow : 
+                Niart : 
+                Hobtre : 
+                Lorsel : 
+                Gidior : 
+                Mulor : 
+                Berifon : 
+                Ziroth : 
+                Walloim : 
+                Sontrifia : 
+                Fukzohin : 
+                Noxilion : 500 B
+                Narloth : 
+                Theriba : 
+                Osintech : 400 B
+                Gorungor : 
+                Alon : 
+                Sorokelm :
+                Sarinset : 
+                Darntof : 
+                Zenalor : 
+
+            }
+        }
+        class Güney_Sideranz : 75 M
+        {
+        }
+        class Hefkirin : 12 M
+        {
+        }
+        class Timbara : 45 M
+        {
+        }
+        class Nyrokos : 25 M
+        {
+        }
+        class Onulorp : 166 M
+        {
+
+        }
+        class Dragzyne : 18 M
+        {
+        }
+        class İnsansı : 25 M
+        {
+        }
+
+    }
+}
+
+
+
